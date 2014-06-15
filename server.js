1d23d352827bbcd685dca18985c26ec9362e32a7
Jure Triglav <juretriglav@gmail.com>
2014-06-15 17:44:17 +0200

    Update README, add simple endpoint for peers.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({port: 9000, path: '/', allow_discovery: true});
 
+console.log('Hello');
+
 server.on('connection', function(id) {
   console.log('Connected: ' + id );
 });
@@ -8,3 +10,11 @@ server.on('connection', function(id) {
 server.on('disconnect', function(id) {
   console.log('Disconnected: ' + id );
 });
+
+// Simple HTTP endpoint for Chord peers
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(Object.keys(server._clients.peerjs)));
+}).listen(9001);
+
