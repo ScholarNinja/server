f91625ac465f8a4170e27593fe973fb31f302142
Jure Triglav <juretriglav@gmail.com>
2014-06-19 00:18:55 +0200

    Update to server logging capabilities. Log number of peers on leave/join, log every request's IP.

--- server.js
+++ server.js
@@ -1,20 +1,46 @@
 var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({port: 9000, path: '/', allow_discovery: true});
 
-console.log('Hello');
+var log = function() {
+  var message = Array.prototype.slice.call(arguments).join(' ');
+  console.log(new Date().toISOString(), message);
+}
+
+log('Hello. This is Scholar Ninja server.');
 
 server.on('connection', function(id) {
-  console.log('Connected: ' + id );
+  log('Connected:', id );
+  logNumberOfPeers();
 });
 
 server.on('disconnect', function(id) {
-  console.log('Disconnected: ' + id );
+  log('Disconnected:', id );
+  logNumberOfPeers();
+});
+
+// Log number of peers
+var logNumberOfPeers = function () {
+  if (server._clients['peerjs']) {
+    var numberOfPeers = Object.keys(server._clients['peerjs']).length
+    log('Current number of peers:', numberOfPeers);
+  }
+};
+
+// Log peer listings
+server._app.pre(function(req, res, next) {
+  log('IP', req.connection.remoteAddress, 'requested', req.path());
+  return next();
 });
 
 // Simple HTTP endpoint for Chord peers
+// Returns array of peer ids, e.g. ['peer1','peer2']
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(Object.keys(server._clients.peerjs)));
+  if(server._clients.peerjs) {
+    res.end(JSON.stringify(Object.keys(server._clients.peerjs)));
+  } else {
+    res.end(JSON.stringify([]));
+  }
 }).listen(9001);
 
